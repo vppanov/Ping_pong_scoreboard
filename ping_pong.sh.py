6d4019a5e85c0d325d5e4851b0302f2f1c15a0f1
vppanov <33857391+vppanov@users.noreply.github.com>
2020-08-02 19:17:20 +0300

    Updates to test turtle module scoring

--- ping_pong.sh.py
+++ ping_pong.sh.py
@@ -1,25 +1,78 @@
+
+from time import sleep
+
+import turtle
+
+
+#variable for the game
+score_a = 0
+score_b = 0
 RedScore = 0
 BlueScore = 0
 
-from time import sleep
-while RedScore <= 21 and BlueScore <= 21:
+
+wn = turtle.Screen()
+wn.title("Scoreboard")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 0)
+pen.write("Who is serving ?")
+
+z = str(input())
+if z == "p":
+    serve = True
+else:
+    serve = False
+        
+#pen.write("{} : {}".format(RedScore, BlueScore), align="center", font=("Courier", 80, "bold"))
+
+
+while  RedScore <= 21 and BlueScore <= 21:
+    wn.update()
+   
+    
+ 
+
     if RedScore == 21:
-        print('RED WINS')
+        pen.write('RED WINS')
         sleep(5)
         RedScore = BlueScore = 0 
+        pen.clear()
     elif BlueScore == 21:
-        print('BLUE WINS')
+        
+        pen.write('BLUE WINS')
         sleep(5)
         BlueScore = RedScore = 0
+        pen.clear()
+        
     else:
+        
         x = str(input())
-        if x == "a":
+        if x == "a" and serve is True:
             RedScore += 1
-            print(RedScore, ":", BlueScore)
-        elif x == "2":
+            pen.clear()
+            pen.write("> {} : {}".format(RedScore, BlueScore), align="center", font=("Courier", 80, "bold"))
+        elif x == "a" and serve is False:
+            RedScore += 1
+            pen.clear()
+            pen.write("{} : {} <".format(RedScore, BlueScore), align="center", font=("Courier", 80, "bold"))
+            
+            
+        elif x == "b":
             BlueScore += 1
-            print(RedScore, ":", BlueScore)
-        elif x == 'REDRESET':
-            RedScore = 3
+            pen.clear()
+            pen.write("{} : {}".format(RedScore, BlueScore), align="center", font=("Courier", 80, "bold"))
+            
+ 
         else:
-            print('Bad Input')
+            pen.clear()
+            pen.write('Bad Input')
