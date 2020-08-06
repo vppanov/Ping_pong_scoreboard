f090144f55d417ab83cecd4df4ed54bbd4622953
vppanov <33857391+vppanov@users.noreply.github.com>
2020-08-06 14:26:20 +0300

    latest

--- temporary/test.py
+++ temporary/test.py
@@ -1,58 +1,59 @@
 from tkinter import *
 
-
-
-
-global counter
-counter = 100
+# environment set up
 root = Tk()
 root.overrideredirect(True)
-my_label = Label(root, text="", font=("Arial", 48))
-my_label.pack()
-
-testVar = StringVar()
-testVar.set(str(counter))
-def exitProgram():
-    print("Exiting")
-
-def counterIn(e):
-    global counter
-    counter += 1
-    testVar.set(str(counter)), e.char
-
-
-y = Label(root, font=("Arial", 48), textvariable=testVar)
-y.pack()
-
-#    my_label.update()
- #   my_label = Label(root, text=counter, font=("Arial", 48))
-  #  my_label.pack()
-
-
-BlueButton = Button(root, text="Blue Point", bg="white", fg="yellow", width=10, height=10, command=counterIn)
-BlueButton.pack()
-
-
-y = Label(root, font=("Arial", 48), textvariable=testVar)
-y.pack()
-
-
-
-def exit(event):
-    root.destroy()
-
 root.title("Scoreboard")
-root.geometry("800x480")
+root.geometry("1024x600")
+root.columnconfigure(1, minsize=15)
 root.configure(background="white")
 
-exitButton = Button(root, text="Exit", command=exitProgram, height=2, width=6)
-exitButton.pack()
-root.bind("i", counterIn)
-root.bind("t", counterIn)
-root.bind("q", exit)
+
+# variables
+counter = 100
+counter2 = 0
+
+# variables convert to StringVar
+testVar = StringVar()
+testVar.set(str(counter))
+testVar2 = StringVar()
+testVar2.set(str(counter2))
+
+# function to monitor key action
+
+
+def keyaction(e):
+    global counter
+    global counter2
+    if e.char == "t":
+        counter += 1
+        testVar.set(str(counter)), e.char
+    elif e.char == "i":
+        counter2 += 1
+        testVar2.set(str(counter2)), e.char
+
+
+# def counterin2(z):
+#    global counter2
+#    counter2 += 1
+#    testVar2.set(str(counter2)), z.char
+
+
+# BlueButton = Button(root, text="Blue Point", bg="white", fg="yellow", width=10, height=10, command=keyaction)
+# BlueButton.pack()
+
+y2 = Label(root, font=("Arial", 48), textvariable=testVar2).place(x=288, y=275)
+
+y = Label(root, font=("Arial", 28), textvariable=testVar).place(x=625, y=275)
+
+
+
+def close(e):
+    root.destroy(), e.char
+
+
+root.bind("<Key>", keyaction)
+# root.bind("i", keyaction)
+# root.bind("t", keyaction)
+root.bind("q", close)
 root.mainloop()
-
-
-
-
-
