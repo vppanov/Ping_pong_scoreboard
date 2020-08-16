952dd8957ad91578fad98137b500ab7880bc55a0
Veleslav Panov <33857391+vppanov@users.noreply.github.com>
2020-08-16 21:53:54 +0300

    Delete os.py

--- system/os.py
+++
@@ -1,9 +0,0 @@
-import os
-import runpy
-
-
-path = os.getcwd()
-path2 = ("." + path)
-full = os.path.join(path, "database.py")
-full = os.path.join(path, "system/stats.db")
-exec(open('system/database.py').read())
